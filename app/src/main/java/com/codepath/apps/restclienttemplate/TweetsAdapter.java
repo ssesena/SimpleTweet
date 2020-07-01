5a04ff73b78d3735a113d30aeb188ccf418955ce
Samuel Sesena <samuelsesena@fb.com>
2020-07-01 14:28:04 -0700

    Added images to timeline and character count to compose tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>{
 
@@ -35,6 +39,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Tweet tweet = tweets.get(position);
+        holder.clearImage();
         holder.bind(tweet);
     }
 
@@ -60,18 +65,50 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         ImageView ivProfileImage;
         TextView tvBody;
         TextView tvScreenName;
+        TextView tvName;
+        TextView tvTimestamp;
+        ImageView ivPicture;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvBody = itemView.findViewById(R.id.tvBody);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvTimestamp = itemView.findViewById(R.id.tvTimestamp);
+            ivPicture = itemView.findViewById(R.id.ivPicture);
         }
 
         public void bind(Tweet tweet) {
             tvBody.setText(tweet.body);
-            tvScreenName.setText(tweet.user.screenName);
+            tvScreenName.setText("@" + tweet.user.screenName);
             Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
+            tvName.setText(tweet.user.name);
+            tvTimestamp.setText("- "+ getRelativeTimeAgo(tweet.createdAt));
+            if (tweet.pictureUrl != null){
+                Glide.with(context).load(tweet.pictureUrl).into(ivPicture);
+            }
+        }
+
+        public void clearImage(){
+            Glide.with(context).clear(ivPicture);
+        }
+
+        public String getRelativeTimeAgo(String rawJsonDate) {
+            String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+            sf.setLenient(true);
+
+            String relativeDate = "";
+            try {
+                long dateMillis = sf.parse(rawJsonDate).getTime();
+                relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                        System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            return relativeDate;
         }
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -13,8 +13,11 @@ public class Tweet {
     public String body;
     public String createdAt;
     public User user;
+    //public JSONArray media;
+    //public JSONObject picture;
+    public String pictureUrl;
 
-    // empty constructor for parceler library
+    // empty constructor for Parceler library
     public Tweet(){
 
     }
@@ -24,6 +27,12 @@ public class Tweet {
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        //tweet.media = jsonObject.getJSONArray("media"); tweet.media.length() > 0; getJSONArray("media").length() > 0)
+        if (jsonObject.getJSONObject("entities").has("media")){
+            //tweet.picture = tweet.media.getJSONObject(0);
+            tweet.pictureUrl = jsonObject.getJSONObject("entities").getJSONArray(("media")).getJSONObject(0).getString("media_url_https");
+            //tweet.picture.getString("media_url_https");
+        }
         return tweet;
     }
 
